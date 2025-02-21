93bd212a9ff944f1e65e97f9ba7baacce9e276ec
Joaquin <quino.l85@gmail.com>
2025-02-21 02:26:49 +0100

    desplegamiento

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 //Rutas para trabajar en deploiment
 export const environment = {
     production: false,
-    authUrl: 'http://localhost:8091/auth',
-    apiUrl: 'http://localhost:8091/api/v1'
+    authUrl: 'http://20.82.105.151:8091/api/v1',
+    apiUrl: 'http://20.82.105.151:8091/api/v1' // Ajusta según tus rutas
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
     production: false,
     authUrl: 'http://localhost:8091/auth',
-    apiUrl: 'http://localhost:8091/api/v1'
+    apiUrl: 'http://20.82.105.151:8091/api/v1' // Ajusta según tus rutas
 };
